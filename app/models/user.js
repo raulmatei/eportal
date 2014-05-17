6857b72e346bd3c170d96b1a972867ead97ad77a
Andrei Cacio <andrei.cacio@evozon.com>
2014-05-17 17:05:59 +0300

    Implemented User model

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,26 @@
 var User = DS.Model.extend({
-  fullName: DS.attr()
+    title: DS.attr(),
+    screenName: DS.attr(),
+    emailAddress: DS.attr(),
+    firstName: DS.attr(),
+    middleName: DS.attr(),
+    lastName: DS.attr(),
+    gender: DS.attr(),
+    jobTitle: DS.attr(),
+    birthday: DS.attr(),
+    address: DS.attr(),
+    phoneNumber: DS.attr(),
+    skype: DS.attr(),
+    facebook: DS.attr(),
+
+    isValid: function () {
+        return (
+        			   Ember.isEmpty(this.get('screenName')) 
+        			|| Ember.isEmpty(this.get('emailAddress')) 
+        			|| Ember.isEmpty(this.get('firstName')) 
+        			|| Ember.isEmpty(this.get('lastName'))
+        		) ? false : true;
+    }.property('screenName', 'emailAddress', 'firstName', 'lastName')
 });
 
 export default User;
--- config/environments/development.coffee
+++ config/environments/development.coffee
@@ -2,8 +2,8 @@ window.ENV.development = true
 
 window.ENV.FIXTURES = {
   user: [
-    { id: 1, fullName: 'Tom Dale' }
-    { id: 2, fullName: 'Yehuda Katz' }
-    { id: 3, fullName: 'Jo Liss' }
+    { id: 1, screenName: 'tom-dale', firstName: 'Tom', lastName: 'Dale', emailAddress: 'tomdale@yahoo.com' }
+    { id: 2, screenName: 'andrei-dale', firstName: 'Andrei', lastName: 'Dale', emailAddress: 'andreidale@yahoo.com'}
+    { id: 3, screenName: 'ion-dale', firstName: 'Ion', lastName: 'Dale', emailAddress: 'ion@yahoo.com' }
   ]
 }
