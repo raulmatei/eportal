9727e47346252766804439b3499024d578090089
Andrei Cacio <andrei.cacio@evozon.com>
2014-05-18 10:05:50 +0300

    Refactored the onliner

--- app/models/user.js
+++ app/models/user.js
@@ -17,10 +17,10 @@ var User = DS.Model.extend({
 
   isValid: function () {
       return (
-      			   Ember.isEmpty(this.get('screenName'))
-      			|| Ember.isEmpty(this.get('emailAddress'))
-      			|| Ember.isEmpty(this.get('firstName'))
-      			|| Ember.isEmpty(this.get('lastName'))
+      			Ember.isEmpty(this.get('screenName'))   ||
+      			Ember.isEmpty(this.get('emailAddress')) ||
+      			Ember.isEmpty(this.get('firstName'))	||
+      			Ember.isEmpty(this.get('lastName'))	    
       		) ? false : true;
   }.property('screenName', 'emailAddress', 'firstName', 'lastName'),
 });
